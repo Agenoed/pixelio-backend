995ec26ae607e04b9d67fbf92f9e451a8dc961f1
Igasus <ihor.hubskyi@gmail.com>
2023-05-26 10:53:02 +0300

    (feat): Added AuthService.

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,27 +1,15 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwtManager = require("../infrastructure/common/jwt.manager");
-const User = require("../infrastructure/entities/user.entity");
+const authService = require("../services/auth.service");
 
 const router = express.Router();
 
 // Register
 router.post("/register", async (req, res, next) => {
     try {
-        var passwordHash = await bcrypt.hash(req.body.password, 10);
-        var user = new User({
-            email: req.body.email,
-            passwordHash: passwordHash
-        });
-    
-        await user.save();
-    
-        var accessToken = jwtManager.generate(user._id, user.email);
-    
-        return res.status(200).json({
-            userId: user._id,
-            accessToken: accessToken
-        });
+        // TODO Add UserCredentials (req.body) validation
+        var authResult = await authService.registerAsync(req.body);
+
+        return res.status(200).json(authResult);
     }
     catch (err) {
         return next(err);
@@ -31,26 +19,10 @@ router.post("/register", async (req, res, next) => {
 // Login
 router.post("/login", async (req, res, next) => {
     try {
-        var user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            return res.status(400).json({
-                message: "Incorrect Email or Password"
-            });
-        }
-    
-        var isPasswordCorrect = await bcrypt.compare(req.body.password, user.passwordHash);
-        if (!isPasswordCorrect) {
-            return res.status(400).json({
-                message: "Incorrect Email or Password"
-            });
-        }
-    
-        var accessToken = jwtManager.generate(user._id, user.email);
-    
-        return res.status(200).json({
-            userId: user._id,
-            accessToken: accessToken
-        });
+        // TODO Add UserCredentials (req.body) validation
+        var authResult = await authService.loginAsync(req.body);
+
+        return res.status(200).json(authResult);
     }
     catch (err) {
         return next(err);
